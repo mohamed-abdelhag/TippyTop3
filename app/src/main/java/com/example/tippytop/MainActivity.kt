9a7a25465c57975a416ffb0fb3110c6c84469011
Dexxxster21 <mohameda.abdelhag@gmail.com>
2023-09-21 09:38:59 +0400

    Initial commit

---
+++ app/src/main/java/com/example/tippytop/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.tippytop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tippytop.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var cajiga: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        cajiga = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(cajiga.root)
+
+        cajiga.calculateButton.setOnClickListener{ calculateTip() }
+
+    }
+
+    private fun calculateTip(){
+        val tipInString = cajiga.costOfService.text.toString()
+
+        val cost = tipInString.toDoubleOrNull()
+        if (cost == null || cost == 0.0 ){
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage =
+            when(cajiga.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+            }
+
+        var tip = cost * tipPercentage
+
+
+        if (cajiga.roundUpSwitch.isChecked){ tip = kotlin.math.ceil(tip)}
+
+        displayTip(tip)
+
+    }
+
+    private fun displayTip(tip: Double) {
+        val tipy = NumberFormat.getCurrencyInstance().format(tip)
+        cajiga.tipResult.text = getString(R.string.tip_amount , tipy)
+
+    }
+
+
+}
