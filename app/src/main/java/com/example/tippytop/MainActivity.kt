b733916759783e888d55d92c93c9a15bb07b8453
Dexxxster21 <mohameda.abdelhag@gmail.com>
2023-09-23 08:58:16 +0400

    polish up and add themes

--- app/src/main/java/com/example/tippytop/MainActivity.kt
+++ app/src/main/java/com/example/tippytop/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tippytop
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tippytop.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -20,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip(){
-        val tipInString = cajiga.costOfService.text.toString()
+        val tipInString = cajiga.costOfServiceEditText.text.toString()
 
         val cost = tipInString.toDoubleOrNull()
         if (cost == null || cost == 0.0 ){
@@ -50,5 +54,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
 
 }
